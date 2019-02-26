07afe7a8c6ecf6229a8576b7d6fc21dcd0453af5
drewheathens <mudavadie@gmail.com>
2019-02-26 12:45:54 +0300

    added navbar,bootstrap3, added directory in settingst to base dir and static

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 import datetime as dt
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def welcome(request):
@@ -9,15 +9,7 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
      # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-news/today-news.html', {"date": date,})
 
 
 def convert_dates(dates):
@@ -31,23 +23,19 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def past_days_news(request,past_date):
+# View Function to present news from past days
+def past_days_news(request, past_date):
 
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-        day = convert_dates(date)
+    if date == dt.date.today():
+        return redirect(news_of_day)
 
-        html = f'''
-	        <html>
-	            <body>
-	                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-	            </body>
-	        </html>
-	            '''
-    return HttpResponse(html)
+    return render(request, 'all-news/past-news.html', {"date": date})
