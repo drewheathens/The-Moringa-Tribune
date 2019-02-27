860186e282e5b6592aa1d13d0d01140267165097
drewheathens <mudavadie@gmail.com>
2019-02-27 18:33:49 +0300

    added upload function

--- news/models.py
+++ news/models.py
@@ -30,6 +30,7 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/')
     
     @classmethod
     def days_news(cls,date):
--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     # url('^$',views.welcome,name = 'welcome'),
@@ -7,6 +9,9 @@ urlpatterns=[
     url(r'^$',views.news_today,name='newsToday'),
     #  return news from those dates
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^article/(\d+)',views.article,name ='article')
 ]
 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
