9adad9c2ac919ca1c65b4a3c47b8d8ea82ff274a
drewheathens <mudavadie@gmail.com>
2019-02-27 11:57:00 +0300

    added selfsave editor function

--- news/models.py
+++ news/models.py
@@ -5,13 +5,19 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    #blank =True allows null values into db
+    phone_number = models.CharField(max_length = 10,blank =True)
 
     def __str__(self):
     	return self.first_name
-    class Meta:
+
+    def save_editor(self):
+        self.save()
+class Meta:
        ordering = ['first_name']
-    class tags(models.Model):
-    	name = models.CharField(max_length =30)
+       
+class tags(models.Model):
+    name = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
@@ -21,7 +27,7 @@ class Article(models.Model):
     post = models.TextField()
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
-    pub_date = models.DateTimeField(auto_now_add=true)
+    pub_date = models.DateTimeField(auto_now_add=True)
     # try:
 #     editor = Editor.objects.get(email = 'example@gmail.com')
 #     print('Editor found')
